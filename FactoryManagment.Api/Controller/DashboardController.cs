a86bc2c130589277e81f35f1760771caec1d81c7
Tarek_H <thesham426@gmail.com>
2026-04-21 12:45:15 +0200

    Added DashboardController/currentActivity-Endpoint

---
+++ FactoryManagment.Api/Controller/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿using FactoryManagment.Application.Abstractions.Interfaces;
+using FactoryManagment.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FactoryManagment.Api.Controller
+{
+    public class DashboardController : ControllerBase
+    {
+        private readonly IDashboardService _dashboardService;
+
+        public DashboardController(IDashboardService dashboardService)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        [HttpGet("current-activity")]
+        public async Task<IActionResult> GetCurrentActivity()
+        {
+            var currentActivity = await _dashboardService.GetCurrentActivityAsync();
+            return Ok(currentActivity);
+        }
+    }
+}
---
+++ FactoryManagment.Domain/Entities/TaskItems.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FactoryManagment.Domain.Entities
+{
+    public class TaskItems
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string TaskName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        public string TaskType { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        public string Status { get; set; } = "Pending";
+
+        [MaxLength(100)]
+        public string MachineName { get; set; } = string.Empty;
+
+        public DateTime StartedAt { get; set; }
+
+        public DateTime? CompletedAt { get; set; }
+    }
+}
