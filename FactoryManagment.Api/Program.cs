7918b3e4f363c9d1196599c0aa6e35ff892567b5
Yousef Saeed <yousaeed2020@gmail.com>
2026-04-22 03:06:57 +0200

    Remove some code in program.cs

--- FactoryManagment.Api/Program.cs
+++ FactoryManagment.Api/Program.cs
@@ -1,27 +1,19 @@
+using FactoryManagment.Api.Middleware;
 using FactoryManagment.Infrastructure;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDependencies(builder.Configuration);
 
-builder.Host.UseSerilog((context, configuration) =>
-    configuration.ReadFrom.Configuration(context.Configuration)
-);
-
 var app = builder.Build();
-
-builder.Services.AddDependencies(builder.Configuration);
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
-app.UseSerilogRequestLogging();
+
 
 app.UseHttpsRedirection();
 
